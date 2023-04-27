eb496795caf717d1e4800a0a6decaf2c67eccff6
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2023-04-27 10:52:07 -0300

    First Commit

---
+++ noticias.js
@@ -0,0 +1,22 @@
+﻿var http = require('http');
+
+var server = http.createServer( function (req, res){
+
+    var categoria = req.url;
+  
+
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Noticias Tecnologia</body></html>");
+    }else if (categoria == '/moda'){
+        res.end("<html><body>Notícias Moda</body></html>");
+    }else if (categoria == '/beleza'){
+        res.end("<html><body>Notícias Beleza</body></html>");
+    }else{
+        res.end("<html><body>Portal de Notícias</body></html>");
+    }
+
+
+
+});
+console.log("Servidor ON");
+server.listen(3000);
